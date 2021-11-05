6a4511eb4156759d87174aa25071fbc204c8279d
Vladimir Grichina <vgrichina@gmail.com>
2021-11-04 23:07:24 -0700

    Compare different ways to load module

--- test.js
+++ test.js
@@ -1,21 +1,34 @@
 const notImplemented = (...args) => {
-  console.error('method not implemented');
-  console.debug('args', args);
-  console.trace();
-  throw new Error('method not implemented');
+    console.error('method not implemented');
+    console.debug('args', args);
+    console.trace();
+    throw new Error('method not implemented');
 };
 
 const imports = {
-  env: {
-    input: notImplemented,
-    register_len: notImplemented,
-    read_register: notImplemented,
-    value_return: notImplemented,
-    panic: notImplemented,
-    abort: notImplemented,
-  }
+    env: {
+        input: notImplemented,
+        register_len: notImplemented,
+        read_register: notImplemented,
+        value_return: notImplemented,
+        panic: notImplemented,
+        abort: notImplemented,
+    }
 }
 
-const fs = require('fs'),
-    wasm = WebAssembly.instantiate(new Uint8Array(fs.readFileSync('./web4.wasm')), imports)
-        .then(result => console.log(result));
+const fs = require('fs');
+const wasmData = new Uint8Array(fs.readFileSync('./web4.wasm'));
+
+(async function() {
+    console.time('wasm compile');
+    const wasmModule = await WebAssembly.compile(wasmData);
+    console.timeEnd('wasm compile');
+
+    console.time('module instantiate');
+    const wasm2 = await WebAssembly.instantiate(wasmModule, imports);
+    console.timeEnd('module instantiate');
+
+    console.time('wasm instantiate');
+    const wasm = await WebAssembly.instantiate(wasmData, imports);
+    console.timeEnd('wasm instantiate');
+})();
