af984725bcfc9b79abc2710e44dd3836fc2def9d
Vladimir Grichina <vgrichina@gmail.com>
2021-11-09 11:52:23 -0800

    Further refactoring of Redis code

--- storage-client.js
+++ storage-client.js
@@ -29,36 +29,40 @@ const withRedis = (name, fn) => async (...args) => {
     }
 }
 
-const getLatestBlockHeight = withRedis('getLatestBlockHeight', redisClient => async () => {
+const getLatestBlockHeight = redisClient => async () => {
     return await redisClient.get('latest_block_height');
-});
+};
 
-const getLatestContractBlockHash = withRedis('getLatestContractBlockHash', redisClient => async (contractId, blockHeight) => {
+const getLatestContractBlockHash = redisClient => async (contractId, blockHeight) => {
     const [contractBlockHash] = await redisClient.sendCommand('ZREVRANGEBYSCORE',
         [Buffer.from(`code:${contractId}`), blockHeight, '-inf', 'LIMIT', '0', '1']);
     return contractBlockHash;
-});
+};
 
-const getContractCode = withRedis('getContractCode', redisClient => async (contractId, blockHash) => {
+const getContractCode = redisClient => async (contractId, blockHash) => {
     return await redisClient.get(Buffer.concat([Buffer.from(`code:${contractId}:`), blockHash]));
-});
+};
 
-const getLatestDataBlockHash = withRedis('getLatestDataBlockHash', redisClient => async (redisKey, blockHeight) => {
+const getLatestDataBlockHash = redisClient => async (redisKey, blockHeight) => {
     redisKey = Buffer.from(redisKey);
-    const [blockHash] = await redisClient.sendCommand('ZREVRANGEBYSCORE', [redisKey, blockHeight, '-inf', 'LIMIT', '0', '1']);
+    const [blockHash] = await redisClient.sendCommand('ZREVRANGEBYSCORE',
+        [redisKey, blockHeight, '-inf', 'LIMIT', '0', '1']);
     return blockHash;
-});
+};
 
-const getData = withRedis('getData', redisClient => async (redisKey, blockHash) => {
+const getData = redisClient => async (redisKey, blockHash) => {
     redisKey = Buffer.from(redisKey);
-    const redisClient = await getRedisClient();   
     return await redisClient.get(Buffer.concat([redisKey, Buffer.from(':'), blockHash]));
-});
+};
 
-module.exports = {
+const exportsList = {
     getLatestBlockHeight,
     getLatestContractBlockHash,
     getContractCode,
     getLatestDataBlockHash,
     getData,
 };
+
+module.exports = Object.keys(exportsList)
+    .map(key => ({ [key]: withRedis(key, exportsList[key]) }))
+    .reduce((a, b) => Object.assign(a, b));
