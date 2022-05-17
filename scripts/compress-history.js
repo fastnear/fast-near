37507fd28f0aff6530277d69ceaf1e4014e9a1b2
Vladimir Grichina <vgrichina@gmail.com>
2022-05-17 04:09:43 -0700

    Add script to compress history with test

---
+++ scripts/compress-history.js
@@ -0,0 +1,25 @@
+const { getLatestBlockHeight, cleanOlderData, scanAllKeys } = require("../storage-client");
+
+async function compressHistory() {
+    const blockHeight = await getLatestBlockHeight();
+    let iterator;
+    do {
+        const [newIterator, keys] = await scanAllKeys(iterator); 
+        await Promise.all(keys.map(async key => {
+            console.log('compress', key.toString('utf8'));
+            await cleanOlderData(key, blockHeight);
+        }));
+        iterator = newIterator;
+    } while (iterator.toString('utf8') != '0');
+}
+
+module.exports = compressHistory;
+
+if (!module.parent) {
+    compressHistory().catch(e => {
+        console.error(e);
+        process.exit(1);
+    }).then(() => {
+        process.exit(0);
+    });
+}
--- storage-client.js
+++ storage-client.js
@@ -11,6 +11,8 @@ let redisCache = new LRU({
 const BLOCK_INDEX_CACHE_TIME = 500;
 const REDIS_URL = process.env.FAST_NEAR_REDIS_URL || 'redis://localhost:6379';
 
+const SCAN_COUNT = 100000;
+
 let redisClient;
 function getRedisClient() {
     if (!redisClient) {
@@ -23,7 +25,9 @@ function getRedisClient() {
     return {
         get: promisify(redisClient.get).bind(redisClient),
         set: promisify(redisClient.set).bind(redisClient),
+        del: promisify(redisClient.del).bind(redisClient),
         zadd: promisify(redisClient.zadd).bind(redisClient),
+        zrem: promisify(redisClient.zrem).bind(redisClient),
         sendCommand: promisify(redisClient.sendCommand).bind(redisClient),
         scan: promisify(redisClient.scan).bind(redisClient),
     };
@@ -37,7 +41,7 @@ const withRedis = ({ name }, fn) => async (...args) => {
     const readableArgs = prettyArgs(args);
     debug(name, ...readableArgs);
     try {
-        const redisClient = getRedisClient();   
+        const redisClient = getRedisClient();
         return await fn(redisClient)(...args);
     } finally {
         debug(`${name} done`, ...readableArgs);
@@ -56,7 +60,7 @@ const withRedisAndCache = ({ name, cachedExpires }, fn) => async (...args) => {
         }
         debug(name, 'local cache miss', cacheKey);
 
-        const redisClient = getRedisClient();   
+        const redisClient = getRedisClient();
         const resultPromise = fn(redisClient)(...args);
         // TODO: Protect from size-bombing cache?
         redisCache.set(cacheKey, resultPromise, cachedExpires && BLOCK_INDEX_CACHE_TIME);
@@ -72,6 +76,7 @@ const getLatestBlockHeight = redisClient => async () => {
 };
 
 const setLatestBlockHeight = redisClient => async (blockHeight) => {
+    redisCache.del('getLatestBlockHeight');
     return await redisClient.set('latest_block_height', blockHeight.toString());
 }
 
@@ -103,14 +108,31 @@ const setData = redisClient => async (compKey, blockHash, blockHeight, data) => 
         .zadd(dataBlockHashKey(compKey), blockHeight, blockHash);
 };
 
-const deleteData = redisClient => async (compKey, blockHash, blockHeight, data) => {
+const deleteData = redisClient => async (compKey, blockHash, blockHeight) => {
     compKey = Buffer.from(compKey);
     await redisClient
         .zadd(dataBlockHashKey(compKey), blockHeight, blockHash);
 };
 
+const cleanOlderData = redisClient => async (compKey, blockHeight) => {
+    compKey = Buffer.from(compKey);
+    const blockHashKey = dataBlockHashKey(compKey);
+    const blockHashes = await redisClient.sendCommand('ZREVRANGEBYSCORE', [blockHashKey, blockHeight, '-inf']);
+    for (let blockHash of blockHashes.slice(1)) {
+        await redisClient.del(dataKey(compKey, blockHash));
+        await redisClient.zrem(blockHashKey, blockHash);
+    }
+}
+
+const scanAllKeys = redisClient => async (iterator) => {
+    const [newIterator, keys] = await redisClient.scan(iterator || 0, 'MATCH', Buffer.from('h:*'), 'COUNT', SCAN_COUNT);
+    return [newIterator, keys.map(k =>
+        k.slice(2) // NOTE: Remove h: prefix
+    )];
+}
+
 const scanDataKeys = redisClient => async (contractId, blockHeight, keyPattern, iterator, limit) => {
-    const [newIterator, keys] = await redisClient.scan(iterator, 'MATCH', Buffer.from(`data:${contractId}:${keyPattern}`), 'COUNT', limit); 
+    const [newIterator, keys] = await redisClient.scan(iterator, 'MATCH', Buffer.from(`data:${contractId}:${keyPattern}`), 'COUNT', limit);
     const data = await Promise.all(keys.map(async key => {
         const compKey = Buffer.from(key).slice('data:'.length);
         const storageKey = compKey.slice(contractId.length + 1);
@@ -126,13 +148,18 @@ const scanDataKeys = redisClient => async (contractId, blockHeight, keyPattern, 
     };
 };
 
+const closeRedis = () => new Promise((resolve, reject) => redisClient.quit(e => e ? reject(e) : resolve()));
+
 module.exports = {
     // TODO: Rely on function name instead?
-    getLatestBlockHeight: withRedisAndCache({ name: 'getLatestBlockHeight', cachedExpires: true }, getLatestBlockHeight), 
+    getLatestBlockHeight: withRedisAndCache({ name: 'getLatestBlockHeight', cachedExpires: true }, getLatestBlockHeight),
     getLatestDataBlockHash: withRedisAndCache({ name: 'getLatestDataBlockHash' }, getLatestDataBlockHash),
     getData: withRedisAndCache({ name: 'getData' }, getData),
     scanDataKeys: withRedisAndCache({ name: 'scanDataKeys' }, scanDataKeys),
     setLatestBlockHeight: withRedis({ name: 'setLatestBlockHeight' }, setLatestBlockHeight),
     setData: withRedis({ name: 'setData' }, setData),
     deleteData: withRedis({ name: 'deleteData' }, deleteData),
+    cleanOlderData: withRedis({ name: 'cleanOlderData' }, cleanOlderData),
+    scanAllKeys: withRedis({ name: 'scanAllKeys' }, scanAllKeys),
+    closeRedis,
 }
