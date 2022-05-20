5318ae3b77c820caa7e9057d023f66b9328a8580
Vladimir Grichina <vgrichina@gmail.com>
2022-05-19 17:11:30 -0700

    Allow to clean up history when loading from near lake

--- scripts/load-from-near-lake.js
+++ scripts/load-from-near-lake.js
@@ -1,16 +1,17 @@
 const { stream } = require('near-lake-framework');
 const bs58 = require('bs58');
 const { serialize } = require('borsh');
-const { setLatestBlockHeight, setData, deleteData } = require('../storage-client');
+const { setLatestBlockHeight, setData, deleteData, cleanOlderData } = require('../storage-client');
 const { accountKey, dataKey, codeKey } = require('../storage-keys');
 const { Account, BORSH_SCHEMA } = require('../data-model');
 
 let totalMessages = 0;
 let timeStarted = Date.now();
 
-async function handleStreamerMessage(streamerMessage) {
+async function handleStreamerMessage(streamerMessage, { historyLength } = {}) {
     const { height: blockHeight, hash: blockHashB58 } = streamerMessage.block.header;
     const blockHash = bs58.decode(blockHashB58);
+    const keepFromBlockHeight = historyLength && blockHeight - historyLength;
     totalMessages++;
     console.log(`Block #${blockHeight} Shards: ${streamerMessage.shards.length} Speed: ${totalMessages * 1000 / (Date.now() - timeStarted)} blocks/second`);
 
@@ -19,35 +20,59 @@ async function handleStreamerMessage(streamerMessage) {
             switch (type) {
                 case 'account_update': {
                     const { accountId, amount, locked, codeHash, storageUsage } = change;
-                    await setData(accountKey(accountId), blockHash, blockHeight,
+                    const compKey = accountKey(accountId);
+                    await setData(compKey, blockHash, blockHeight,
                         serialize(BORSH_SCHEMA, new Account({ amount, locked, code_hash: bs58.decode(codeHash), storage_usage: storageUsage })))
+                    if (keepFromBlockHeight) {
+                        await cleanOlderData(compKey, keepFromBlockHeight);
+                    }
                     break;
                 }
                 case 'account_deletion': {
                     const { accountId } = change;
-                    await deleteData(accountKey(accountId), blockHash, blockHeight);
+                    const compKey = accountKey(accountId);
+                    await deleteData(compKey, blockHash, blockHeight);
+                    if (keepFromBlockHeight) {
+                        await cleanOlderData(compKey, keepFromBlockHeight);
+                    }
                     break;
                 }
                 case 'data_update': {
                     const { accountId, keyBase64, valueBase64 } = change;
                     const storageKey = Buffer.from(keyBase64, 'base64');
-                    await setData(dataKey(accountId, storageKey), blockHash, blockHeight, Buffer.from(valueBase64, 'base64'));
+                    const compKey = dataKey(accountId, storageKey);
+                    await setData(compKey, blockHash, blockHeight, Buffer.from(valueBase64, 'base64'));
+                    if (keepFromBlockHeight) {
+                        await cleanOlderData(compKey, keepFromBlockHeight);
+                    }
                     break;
                 }
                 case 'data_deletion': {
                     const { accountId, keyBase64 } = change;
                     const storageKey = Buffer.from(keyBase64, 'base64');
-                    await deleteData(dataKey(accountId, storageKey), blockHash, blockHeight);
+                    const compKey = dataKey(accountId, storageKey);
+                    await deleteData(compKey, blockHash, blockHeight);
+                    if (keepFromBlockHeight) {
+                        await cleanOlderData(compKey, keepFromBlockHeight);
+                    }
                     break;
                 }
                 case 'contract_code_update': {
                     const { accountId, codeBase64 } = change;
-                    await setData(codeKey(accountId), blockHash, blockHeight, Buffer.from(codeBase64, 'base64'));
+                    const compKey = codeKey(accountId);
+                    await setData(compKey, blockHash, blockHeight, Buffer.from(codeBase64, 'base64'));
+                    if (keepFromBlockHeight) {
+                        await cleanOlderData(compKey, keepFromBlockHeight);
+                    }
                     break;
                 }
                 case 'contract_code_deletion': {
                     const { accountId } = change;
-                    await deleteData(codeKey(accountId), blockHash, blockHeight);
+                    const compKey = codeKey(accountId);
+                    await deleteData(compKey, blockHash, blockHeight);
+                    if (keepFromBlockHeight) {
+                        await cleanOlderData(compKey, keepFromBlockHeight);
+                    }
                     break;
                 }
             }
@@ -76,10 +101,14 @@ yargs(process.argv.slice(2))
                     describe: 'how many blocks to try fetch in parallel',
                     number: true,
                     default: DEFAULT_BATCH_SIZE
+                })
+                .option('history-length', {
+                    describe: 'How many latest blocks of history to keep. Unlimited by default.',
+                    number: true
                 }),
             async argv => {
 
-        const { startBlockHeight, bucketName, regionName, endpoint, batchSize } = argv;
+        const { startBlockHeight, bucketName, regionName, endpoint, batchSize, historyLength } = argv;
 
         for await (let streamerMessage of stream({
             startBlockHeight: startBlockHeight || await storageClient.getLatestBlockHeight() || 0,
@@ -88,7 +117,7 @@ yargs(process.argv.slice(2))
             s3Endpoint: endpoint,
             blocksPreloadPoolSize: batchSize
         })) {
-            await handleStreamerMessage(streamerMessage);
+            await handleStreamerMessage(streamerMessage, { historyLength });
         }
     })
     .parse();
