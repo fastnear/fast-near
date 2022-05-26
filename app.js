384223f0360af4a03338585d14be23ba2d19282b
Vladimir Grichina <vgrichina@gmail.com>
2022-05-25 18:27:40 -0700

    Add /healthz check

--- app.js
+++ app.js
@@ -100,6 +100,19 @@ router.get('/account/:accountId/data/:keyPattern', async ctx => {
     };
 });
 
+const MAX_BLOCK_LAG_TIME_MS = 20000;
+router.get('/healthz', async ctx => {
+    const latestBlockHeight = await storageClient.getLatestBlockHeight();
+    const latestBlockTimestamp = (await storageClient.getBlockTimestamp(latestBlockHeight)) / 1000000;
+
+    // NOTE: fast-near node considered unhealthy if it's out of sync
+    if (Date.now() - latestBlockTimestamp > MAX_BLOCK_LAG_TIME_MS) {
+        ctx.throw(500, 'unhealthy (out of sync)');
+    }
+
+    ctx.status = 204; // No Content
+});
+
 const { proxyJson, handleJsonRpc } = require('./json-rpc');
 router.post('/', handleJsonRpc);
 router.get('/(status|metrics|health)', proxyJson);
