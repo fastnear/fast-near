df5d004ba39c06b0ef597561b92de5d32fb491ed
Vladimir Grichina <vgrichina@gmail.com>
2022-05-29 00:22:44 -0700

    Add compress-history run as e2e test

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Try importing small chunk of real mainnet data
 node scripts/load-from-near-lake.js near-lake-data-mainnet --batch-size 10 --history-length 1 --start-block-height 66377251 --limit 5
+node scripts/compress-history.js
 
 
