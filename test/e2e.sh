adbad6b103a083a6e2c610bd35fc5331b60a4b9b
Vladimir Grichina <vgrichina@gmail.com>
2022-05-28 23:48:03 -0700

    Add basic end to end test for loading data

---
+++ test/e2e.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Try importing small chunk of real mainnet data
+node scripts/load-from-near-lake.js near-lake-data-mainnet --batch-size 10 --history-length 1 --start-block-height 66377251 --limit 5
+
+
