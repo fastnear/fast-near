1b5191812bc2cc5f49ff2450a2c2ef2e54ab03fc
Vladimir Grichina <vgrichina@gmail.com>
2022-05-29 02:40:02 -0700

    Add basic JSON-RPC e2e test (view account)

--- test/e2e.sh
+++ test/e2e.sh
@@ -3,6 +3,11 @@ set -ex
 
 # Try importing small chunk of real mainnet data
 node scripts/load-from-near-lake.js near-lake-data-mainnet --batch-size 10 --history-length 1 --start-block-height 66377251 --limit 5
+
+# Try compressing history
 node scripts/compress-history.js
 
-
+# Use near-cli for basic JSON-RPC tests
+yarn dev &
+npx near-cli state aurora --nodeUrl http://localhost:3000
+kill %-
