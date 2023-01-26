9506eaca951b600854ad8aa0ad3d306ae10169a1
Vladimir Grichina <vgrichina@gmail.com>
2023-01-26 00:01:31 -0800

    Allow to select storage type using FAST_NEAR_STORAGE_TYPE env var

--- storage/index.js
+++ storage/index.js
@@ -3,4 +3,6 @@
 const { RedisStorage } = require('./redis');
 const { LMDBStorage } = require('./lmdb-embedded');
 
-module.exports = new LMDBStorage();
+const storageType = process.env.FAST_NEAR_STORAGE_TYPE || 'redis';
+
+module.exports = storageType === 'lmdb' ? new LMDBStorage() : new RedisStorage();
