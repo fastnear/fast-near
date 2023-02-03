ab6ef0d3ef4f794bae04f89a9880c35092b23f6d
Vladimir Grichina <vgrichina@gmail.com>
2023-02-02 18:19:12 -0800

    Change buffer pretty printing

--- utils/pretty-buffer.js
+++ utils/pretty-buffer.js
@@ -1,10 +1,11 @@
-const textDecoder = new TextDecoder('utf8', { fatal: true });
 const prettyBuffer = buffer => {
-    try {
-        return textDecoder.decode(buffer);
-    } catch (e) {
-        return buffer.toString('hex');
-    }
+    return Array.from(buffer).map(c => {
+        if (c >= 32 && c <= 126) {
+            return String.fromCharCode(c);
+        } else {
+            return `\\x${c.toString(16).padStart(2, '0')}`;
+        }
+    }).join('');
 };
 
 module.exports = prettyBuffer;
