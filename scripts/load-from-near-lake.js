e2a284fe33615d20fa96e13d302210b632b1ee36
Vladimir Grichina <vgrichina@gmail.com>
2023-02-06 14:58:24 -0800

    Fix days formatting

--- scripts/load-from-near-lake.js
+++ scripts/load-from-near-lake.js
@@ -17,7 +17,7 @@ function formatDuration(milliseconds) {
     let seconds = Math.floor((milliseconds / 1000) % 60);
     let minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
     let hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
-    let days = Math.floor((milliseconds / (1000 * 60 * 60 * 24)) % 24);
+    let days = Math.floor((milliseconds / (1000 * 60 * 60 * 24)));
     return [days, hours, minutes, seconds].map(n => n.toString().padStart(2, '0')).join(':');
 }
 
