c9e49b136b60d3a74aab61594a475e190d396577
Vladimir Grichina <vgrichina@gmail.com>
2023-02-09 19:09:46 -0800

    Add FAST_NEAR_ENABLE_CACHE env var

--- storage/index.js
+++ storage/index.js
@@ -7,9 +7,14 @@ const { CachedStorage } = require('./cached');
 
 const storageType = process.env.FAST_NEAR_STORAGE_TYPE || 'redis';
 
+const ENABLE_CACHE = ['no', 'false', '0'].indexOf((process.env.FAST_NEAR_ENABLE_CACHE || 'true').trim().toLowerCase()) === -1;
+
+const debugStorage = new DebugStorage(
+    storageType === 'lmdb'
+        ? new LMDBStorage()
+        : new RedisStorage());
+
 module.exports = 
-    new CachedStorage(
-        new DebugStorage(
-            storageType === 'lmdb'
-                ? new LMDBStorage()
-                : new RedisStorage()));
+    ENABLE_CACHE
+        ? new CachedStorage(debugStorage)
+        : debugStorage;
