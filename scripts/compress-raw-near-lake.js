c2dad21c18ebe91d1fe94b83c532d2cf2afbdffa
Vladimir Grichina <vgrichina@gmail.com>
2024-01-06 00:54:41 -0800

    Use 5 files per archive by default

--- scripts/compress-raw-near-lake.js
+++ scripts/compress-raw-near-lake.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const compressing = require('compressing');
 const { pipeline } = require('stream/promises');
-const { config } = require('yargs');
 
-const FILES_PER_ARCHIVE = 100;
+const FILES_PER_ARCHIVE = 5;
 
 function normalizeBlockHeight(number) {
     return number.toString().padStart(12, '0');
@@ -12,7 +11,6 @@ function normalizeBlockHeight(number) {
 async function compress(bucketName, startAfter, limit) {
     for (let i = 0; i < limit; i += FILES_PER_ARCHIVE) {
         console.log(i);
-        const files = [];
 
         // TODO: Make dynamic
         const numShards = 4;
