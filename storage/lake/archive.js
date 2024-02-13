0d1977531e9645958518d1d003fba77cebd8b538
Vladimir Grichina <vgrichina@gmail.com>
2024-02-13 02:43:30 -0800

    Less spammy block progress

--- storage/lake/archive.js
+++ storage/lake/archive.js
@@ -3,17 +3,22 @@ const zlib = require('zlib');
 const tar = require('tar-stream');
 
 const FILES_PER_ARCHIVE = 5;
+const BLOCKS_PER_LOG = 1000;
 
 async function *readBlocks(dataDir, shard, startBlockNumber, endBlockNumber) {
     startBlockNumber = startBlockNumber ? Math.floor(startBlockNumber / FILES_PER_ARCHIVE) * FILES_PER_ARCHIVE : 0;
 
+    const startTime = Date.now();
     for (let blockNumber = startBlockNumber; blockNumber < endBlockNumber; blockNumber += FILES_PER_ARCHIVE) {
-        console.log('blockNumber', blockNumber, 'endBlockNumber', endBlockNumber);
         const blockHeight = normalizeBlockHeight(blockNumber);
         const [prefix1, prefix2] = blockHeight.match(/^(.{6})(.{3})/).slice(1);
         const inFolder = `${dataDir}/${shard}/${prefix1}/${prefix2}`;
         const inFile = `${inFolder}/${blockHeight}.tgz`;
-        console.log('inFile', inFile);
+
+        if (blockNumber > startBlockNumber && blockNumber % BLOCKS_PER_LOG === 0) {
+            const blocksPerSecond = (blockNumber - startBlockNumber) / ((Date.now() - startTime) / 1000);
+            console.log(`Reading block ${blockNumber}. Speed: ${blocksPerSecond.toFixed(2)} blocks/s. ETA: ${(endBlockNumber - blockNumber) / blocksPerSecond} s`);
+        }
 
         const extract = tar.extract();
         const gunzip = zlib.createGunzip();
