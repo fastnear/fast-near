bee9f69b47b4d151f20d44986d2b0a56b8e89e52
Vladimir Grichina <vgrichina@gmail.com>
2024-02-13 20:41:26 -0800

    Refactor roundtrip test a bit

--- test/changes-index.test.js
+++ test/changes-index.test.js
@@ -3,21 +3,28 @@ const fs = require('fs/promises');
 
 const { writeChangesFile, readChangesFile, mergeChangesFiles } = require('../storage/lake/changes-index');
 
-test('roundtrip', async t => {
-    await roundtrip(t, 'app.nearcrowd.near.dat');
-    await roundtrip(t, 'asset-manager.orderly-network.near.dat');
-    await roundtrip(t, 'changes.dat');
-});
-
-async function roundtrip(t, fileName) {
-    const indexFileName = `test/data/lake/index/${fileName}`;
-    const changes = await readStream(await readChangesFile(indexFileName));
-    const tempFileName = `${indexFileName}.tmp`;
-    await writeChangesFile(tempFileName, convertChanges(changes));
-    const tempChanges = await readStream(await readChangesFile(tempFileName));
-    t.deepEqual(tempChanges, changes);
-    t.ok((await fs.readFile(tempFileName)).equals(await fs.readFile(indexFileName)));
+function roundtripTest(test, fileName) {
+    test(`roundtrip ${fileName}`, async t => {
+        const indexFileName = `test/data/lake/index/${fileName}`;
+        const changes = await readStream(await readChangesFile(indexFileName));
+        const tempFileName = `${indexFileName}.tmp`;
+        await writeChangesFile(tempFileName, convertChanges(changes));
+        const tempChanges = await readStream(await readChangesFile(tempFileName));
+        t.deepEqual(tempChanges, changes);
+        t.ok((await fs.readFile(tempFileName)).equals(await fs.readFile(indexFileName)));
+    });
 }
+function roundtrip(fileName) {
+    roundtripTest(test, fileName);
+}
+roundtrip.only = function(fileName) {
+    roundtripTest(test.only, fileName);
+};
+
+roundtrip('app.nearcrowd.near.dat');
+roundtrip('asset-manager.orderly-network.near.dat');
+roundtrip('changes.dat');
+
 
 async function readStream(stream) {
     const chunks = [];
