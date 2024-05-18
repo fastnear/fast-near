0177af92dd469b0c816d1a1aca9f0b67b490b657
Vladimir Grichina <vgrichina@gmail.com>
2024-05-17 20:04:02 -0700

    Parallelize work until final block fetched from neardata
    After that just go sequentially

--- source/neardata.js
+++ source/neardata.js
@@ -1,12 +1,17 @@
 
 // TODO: Allow to break the loop if the user wants to stop reading blocks. Use an AbortController signal?
 async function* readBlocks({ baseUrl = 'https://mainnet.neardata.xyz/v0', startBlockHeight, endBlockHeight, batchSize }) {
+    async function fetchBlockNumber(path) {
+        const res = await fetch(`${baseUrl}/${path}`, { redirect: 'manual' });
+        // TODO: Should handle 404, etc?
+        // Parse location header looking like `location: /v0/block/9820210`
+        return parseInt(res.headers.get('location').split('/').pop(), 10);
+    }
+
     if (!startBlockHeight) {
         // TODO: Change to use limit parameter as otherwise endBlockHeight is not correct here?
         // Fetch the first block height from the API.
-        const res = await fetch(`${baseUrl}/first_block`, { redirect: 'manual' });
-        // Parse location header looking like `location: /v0/block/9820210`
-        startBlockHeight = parseInt(res.headers.get('location').split('/').pop(), 10);
+        startBlockHeight = await fetchBlockNumber('first_block');
     }
 
     const fetchBlock = async (blockHeight) => {
@@ -16,7 +21,9 @@ async function* readBlocks({ baseUrl = 'https://mainnet.neardata.xyz/v0', startB
     };
 
     const workPool = [];
-    for (let blockHeight = startBlockHeight; !endBlockHeight || blockHeight < endBlockHeight; blockHeight++) {
+    let blockHeight = startBlockHeight;
+    let finalBlockHeight = await fetchBlockNumber('last_block/final');
+    for (; !endBlockHeight || blockHeight < Math.min(finalBlockHeight, endBlockHeight); blockHeight++) {
         while (workPool.length >= batchSize) {
             const block = await workPool.shift();
             if (block) {
@@ -25,6 +32,24 @@ async function* readBlocks({ baseUrl = 'https://mainnet.neardata.xyz/v0', startB
         }
 
         workPool.push(fetchBlock(blockHeight));
+
+        if (blockHeight % batchSize === 0) {
+            finalBlockHeight = await fetchBlockNumber('last_block/final');
+        }
+    }
+
+    console.log('finishing work');
+    while (workPool.length > 0) {
+        const block = await workPool.shift();
+        if (block) {
+            yield block;
+        }
+    }
+
+    console.log('fetching more');
+    for (; !endBlockHeight || blockHeight < endBlockHeight; blockHeight++) {
+        const block = await fetchBlock(blockHeight);
+        yield block;
     }
 }
 
